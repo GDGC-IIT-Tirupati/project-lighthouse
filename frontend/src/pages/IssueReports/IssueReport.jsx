38d2ad05d2ffdb3b7adfc9b9c6e71a2d8a800235
AnanthRamOmkar <ananthramomkarkprabhuu@gmail.com>
2026-04-19 01:33:32 +0530

    update IssueReport

--- frontend/src/pages/IssueReports/IssueReport.jsx
+++ frontend/src/pages/IssueReports/IssueReport.jsx
@@ -10,6 +10,17 @@ function IssueReport() {
           <div className={styles.my}>MY</div>
           <div className={styles.report}>TICKETS</div>
         </div>
+
+        <div className={styles.information}>
+            <div className= {styles.tagline}>
+                Track the status of the issues you have flagged across the campus. Every report helps us build a brighter, safer environment for everyone.
+            </div>
+            <div className={styles.total_issues}>
+                <div className = {styles.nos}>14</div>
+                <div className= {styles.text_for_nos}>TOTAL ISSUES LOGGED </div>
+            </div>
+        </div>
+
       </div>
     </>
   );
