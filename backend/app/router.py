3e56eccb22c294ba4d83fc1ae315e4f45b394845
Aasish <50045306+KyberNull@users.noreply.github.com>
2026-04-18 11:52:40 +0000

    Add basic endpoints to router.py

--- backend/app/router.py
+++ backend/app/router.py
@@ -1,7 +1,22 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
-from app.user import get_token
-router=APIRouter()
+from .user import get_token
+
+
+router = APIRouter()
+
 @router.get("/user")
 async def get_userid(user: Annotated[dict, Depends(get_token)]):
     return {"id":user["uid"]}
+
+@router.get("/")
+async def root():
+    return {"message": "Hello from backend!"}
+
+@router.get("/login")
+async def login():
+    return {"message": "Login page"}
+
+@router.get("/register")
+async def register():
+    return {"message": "Register page"}
