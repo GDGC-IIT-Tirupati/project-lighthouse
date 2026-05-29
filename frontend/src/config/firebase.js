f63b166f61b0cc0e6bb2aaeeaf7ff289c37b51c0
SirLancelot-13 <pragyansrivastava13@gmail.com>
2026-05-29 18:02:49 +0530

    updated /config/firebase.js and added .env.example files to backend and frontend

--- frontend/src/config/firebase.js
+++ frontend/src/config/firebase.js
@@ -1,15 +1,15 @@
-import {initializeApp} from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyAL47hd8WeoRhiMg9EcDw9cCRvIXaI3ndY",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "lighthouse-test-d4806.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "lighthouse-test-d4806",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "lighthouse-test-d4806.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "883544657664",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:883544657664:web:11aea6d96f03b0c6ca6de5",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-YNPX68GJP8",
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth  = getAuth(app);
+export const auth = getAuth(app);
