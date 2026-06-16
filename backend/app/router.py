d74a3d9aa0f4a6d6920081980c5f9d27888a5765
SirLancelot-13 <pragyansrivastava13@gmail.com>
2026-06-16 09:23:38 +0530

    Resolved router.py

--- backend/app/router.py
+++ backend/app/router.py
@@ -28,13 +28,10 @@ async def login(payload: LoginRequest, response: Response, db: Session = Depends
 async def register(payload: RegisterRequest, response: Response, db: Session = Depends(get_db)):
     return firebase_register(payload, response, db)
 
-<<<<<<< Updated upstream
 @router.post("/new_ticket", response_model = ChatResponse)
 async def get_model_response(payload: ChatPayload):
     raw_ai_reply = model_response(payload)
     return {'reply': raw_ai_reply}
-=======
 @router.get("/issues")
 async def issues_endpoint(user = Depends(get_current_user), db: Session = Depends(get_db)):
     return get_issues(user, db)
->>>>>>> Stashed changes
